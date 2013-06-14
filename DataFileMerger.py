c76215870a789f4a8c562744ef054fb70ad82433
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-06-14 11:03:02 -0300

    make the file merger more general and runnable from command prompt

--- DataFileMerger.py
+++ DataFileMerger.py
@@ -5,6 +5,8 @@ Created on Thu Mar 21 12:36:17 2013
 @author: hok1
 """
 
+import sys
+
 # Assuming the first line is the header, and all files have the same header
 class DataFileMerger():
     def __init__(self):
@@ -33,10 +35,18 @@ class DataFileMerger():
     def clearList(self):
         self.fileList = []
         
-def mergeFiles(inputFilePrefix, outputFilename):
+def mergeFiles(inputFileList, outputFilename):
     merger = DataFileMerger()
-    mutRates = range(0, 100, 5)
-    for mutRate in mutRates:
-        inputFilename = inputFilePrefix+'MutRate'+str(mutRate)+'.txt'
-        merger.addFile(inputFilename)
+    for filename in inputFileList:
+        merger.addFile(filename)
     merger.merge(outputFilename)
+
+def help():
+    print 'Usage: python DataFileMerger.py <inputFileWildCard> <outputFilename>'
+    
+if __name__ == '__main__':
+    argvs = sys.argv
+    if len(argvs) >= 3:
+        mergeFiles(argvs[1:-1], argvs[-1])
+    else:
+        help()
